ff920cf41f986b1b91db900e6cce1610e8665843
kaareltiitt6nts <kaarel-tiit.tonts@voco.ee>
2025-10-01 11:06:16 +0000

    artikli muutmise ja kustutamise backend

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -39,3 +39,68 @@ export const getArticleBySlug = (req, res) => {
       return res.status(500).json({ message: err })
     })
 }
+
+export const createArticle = (req, res) => {
+  const name = req.body.name
+  const slug = req.body.slug
+  const image = req.body.image
+  const body = req.body.body
+
+  models.Articles.create({
+    name: name,
+    slug: slug,
+    image: image,
+    body: body,
+    published: new Date().toISOString().slice(0, 19).replace("T", " ")
+  })
+  .then(data => {
+    return res.status(200).json({message: "Article created"})
+  })
+  .catch(err => {
+    return res.status(500).json({message: `Failed to create article: ${err}`})
+  })
+}
+
+export const editArticle = (req, res) => {
+  const id = req.body.id
+  const { name, slug, image, body } = req.body
+
+  models.Articles.update(
+    {
+      name: name,
+      slug: slug,
+      image: image,
+      body: body,
+      updatedAt: new Date().toISOString().slice(0, 19).replace("T", " ")
+    },
+    {
+      where: { id: id }
+    }
+  )
+  .then(result => {
+    if (result[0] === 0) {
+      return res.status(404).json({ message: "Article not found or nothing to update" })
+    }
+    return res.status(200).json({ message: "Article edited" })
+  })
+  .catch(err => {
+    return res.status(500).json({ message: `Failed to edit article: ${err}` })
+  })
+}
+
+
+export const deleteArticle = (req, res) => {
+  const id = req.body.id
+
+  models.Articles.destroy({
+    where: {
+      id: id
+    }
+  })
+  .then(data => {
+    return res.status(200).json({message: "Article deleted"})
+  })
+  .catch(err => {
+    return res.status(500).json({message: `Failed to delete article: ${err}`})
+  })
+}
--- routes/articleRouter.js
+++ routes/articleRouter.js
@@ -1,7 +1,11 @@
 import express from "express"
-import { getAllArticles, getArticleBySlug } from "../controllers/articleController.js"
+import { createArticle, deleteArticle, editArticle, getAllArticles, getArticleBySlug } from "../controllers/articleController.js"
 
 export const articleRouter = express.Router()
 
 articleRouter.get("/", getAllArticles)
 articleRouter.get("/:slug", getArticleBySlug)
+
+articleRouter.post("/admin/create/", createArticle)
+articleRouter.put("/admin/edit/", editArticle)
+articleRouter.delete("/admin/delete/", deleteArticle)
